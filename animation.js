bfccf9099ada3292fd5d59fc94207a8c95926779
AndreMarrazes <andre.marrazes301@gmail.com>
2024-04-13 10:32:35 +0100

    home animation

--- animation.js
+++ animation.js
@@ -16,20 +16,19 @@ gsap.ticker.lagSmoothing(0);
 
 gsap.from(".intro__center",{duration:1, opacity: 0,ease:"power1"});
 
+//introduçao home
 
 gsap.set(".introduction",{
     opacity:0,
     yPercent:-100,
-    
-
-   
+      
 });
 
 gsap.to(".introduction", {
     scrollTrigger:{
-        trigger: ".introduction",
-        start:"top center",
-        markers:true,
+      trigger: ".introduction",
+      start:"top center",
+      markers:true,
         
     },
     duration:1.5,
@@ -37,3 +36,107 @@ gsap.to(".introduction", {
     yPercent:0
 });
 
+gsap.set(".Pdfbutton_background_home",{
+  opacity:0,
+  
+});
+
+gsap.to(".Pdfbutton_background_home", {
+  scrollTrigger:{
+    trigger: ".Pdfbutton_background_home",
+    start:"top bottom",
+    markers:true,
+      
+  },
+  duration:1.5,
+  delay:0.5,
+  opacity:1,
+});
+
+//goals home
+gsap.set(".practical",{
+  opacity:0,
+  xPercent:-100,
+});
+
+gsap.to(".practical", {
+  scrollTrigger:{
+    trigger: ".cheap",
+    start:"bottom bottom",
+    markers:true,
+      
+  },
+  duration:1.5,
+  opacity:1,
+  xPercent:0,
+});
+
+gsap.set(".cheap",{
+  opacity:0,
+  
+});
+
+gsap.to(".cheap", {
+  scrollTrigger:{
+    trigger: ".cheap",
+    start:"bottom bottom",
+    markers:true,
+      
+  },
+  duration:1.5,
+  opacity:1,
+});
+
+gsap.set(".efficient",{
+  opacity:0,
+  xPercent:100,
+});
+
+gsap.to(".efficient", {
+  scrollTrigger:{
+    trigger: ".cheap",
+    start:"bottom bottom",
+    markers:true,
+      
+  },
+  duration:1.5,
+  opacity:1,
+  xPercent:0
+});
+
+//team home
+gsap.set(".team_home h2",{
+  opacity:0,
+  yPercent:100,
+    
+});
+
+gsap.to(".team_home h2", {
+  scrollTrigger:{
+    trigger: ".team_home",
+    start:"center bottom",
+    markers:true,
+      
+  },
+  duration:1.5,
+  opacity:1,
+  yPercent:0
+});
+
+//partner home
+gsap.set(".partners_home__img",{
+  opacity:0,
+    
+});
+
+gsap.to(".partners_home__img", {
+  scrollTrigger:{
+    trigger: ".partners_home__img",
+    start:"center bottom",
+    markers:true,
+      
+  },
+  duration:1.5,
+  opacity:1,
+  yPercent:0
+});
